c3f7b16a505da2cb6518cab475e2a037d5d30aa7
Ben Trainor <ben.trainor@my.uu.edu>
2022-04-20 14:24:12 -0500

    Portraits and nature photos done

---
+++ js/photos.js
@@ -0,0 +1,18 @@
+
+var portraits = document.getElementsByClassName("photo-portrait");
+var naturePhotos = document.getElementsByClassName("photo-nature");
+var sourceFile;
+
+for (let i = 0; i < portraits.length; i++) {
+    sourceFile = portraits.item(i).firstElementChild.innerHTML;
+    console.log("Setting background image to " + sourceFile);
+    portraits.item(i).style.backgroundImage = "url(" + sourceFile + ")";
+    portraits.item(i).parentElement.classList.add("col-lg-4", "col-sm-6", "p-0");
+}
+
+for (let i = 0; i < naturePhotos.length; i++) {
+    sourceFile = naturePhotos.item(i).firstElementChild.innerHTML;
+    console.log("Setting background image to " + sourceFile);
+    naturePhotos.item(i).style.backgroundImage = "url(" + sourceFile + ")";
+    naturePhotos.item(i).parentElement.classList.add("col-lg-6", "p-0");
+}
